afa61b7f188cc53f909954a550c0a07b262fba72
Dmytro-Ovch <ovch.dmytro.de@gmail.com>
2025-09-14 23:45:50 +0200

    deployment

--- src/pages/Battle.jsx
+++ src/pages/Battle.jsx
@@ -144,7 +144,8 @@ const Battle = () => {
     // send score to the backend
     if (user?.username) {
       try {
-        await fetch(`${import.meta.env.VITE_API_URL}/leaderboard/update`, {
+        const baseUrl = import.meta.env.VITE_API_URL || "https://pokemon-backend-bccr.onrender.com";
+        await fetch(`${baseUrl}/leaderboard/update`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
