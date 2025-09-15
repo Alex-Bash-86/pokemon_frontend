2ffceb63b275e561789d90c6f3b53f1facd6e346
Alexander Bashenow <bashenow.alexander@gmail.com>
2025-09-15 09:37:17 +0200

    Update ProtectedLayout.jsx

--- src/Layout/ProtectedLayout.jsx
+++ src/Layout/ProtectedLayout.jsx
@@ -1,5 +1,5 @@
 import{ useContext, useEffect } from "react";
-import { AuthContext } from "../contexts/AuthContext.jsx";
+import AuthContext from "../contexts/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
 
 
